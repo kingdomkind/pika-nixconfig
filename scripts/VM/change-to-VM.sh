c3c50f512b8aadc0754ddff5f6f832b462e8d279
kingdomkind <pikadevs@proton.me>
2024-04-08 12:09:38 +0100

    changed hyprland config from home.file to hyprland.conf in it's regular file, hot reload works again, also fixed path issues

--- scripts/VM/change-to-VM.sh
+++ scripts/VM/change-to-VM.sh
@@ -4,9 +4,9 @@ LIBVIRT_DEFAULT_URI=qemu:///system virsh start "Windows-GPU"
 line=$(pgrep looking-glass)
 
 if test -z "$line"; then
-  hyprctl dispatch exec "[workspace 3; fullscreen ]" looking-glass-client
+  hyprctl dispatch exec "[workspace 11; fullscreen]" looking-glass-client
 else
-  hyprctl dispatch workspace 3
+  hyprctl dispatch workspace 11
 fi
 
 hyprctl dispatch submap vm
