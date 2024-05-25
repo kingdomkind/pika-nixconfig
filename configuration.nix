25ae65f270e5636e370ac9e82fe2e3ead540f269
kingdomkind <pikadevs@proton.me>
2024-05-25 18:45:59 +0100

    updated flake & disabled gpu on codium to allow it to work

--- configuration.nix
+++ configuration.nix
@@ -17,14 +17,6 @@
     nvidiaSettings = true;
 
     package = config.boot.kernelPackages.nvidiaPackages.beta;
-
-#package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-#  version = "555.42.02";
-#  sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
-#  openSha256 = "sha256-3/eI1VsBzuZ3Y6RZmt3Q5HrzI2saPTqUNs6zPh5zy6w=";
-#  settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
-#  persistencedSha256 = "";
-#};
   };
   
   # Bootloader.
